8f3fd68751a6702ea127381839a80982e2ce99a7
Hudson-websis <153659178+Hudson-websis@users.noreply.github.com>
2023-12-14 17:38:22 -0300

    Exemplo de mais um código

--- app.js
+++ app.js
@@ -3,3 +3,4 @@ console.log("aplicalçao está se conectando ao banco de dados")
 //mais um console de teste:
 console.log("websis fez um commit sem você ver")
 console.log("Enviando para o GitHub")
+console.log("Exemplo de mais um código")
