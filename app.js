db3e7ceb8a22b5e3a265f6250a27ec35b5a29e5d
Hudson-websis <153659178+Hudson-websis@users.noreply.github.com>
2023-12-14 15:46:57 -0300

    Atualizando o init do projeto

--- app.js
+++ app.js
@@ -1,1 +1,2 @@
-consolo.log("Rodando o sistema de gerenciamento de jogos")
+console.log("Rodando o sistema de gerenciamento de jogos")
+console.log("aplicalçao está se conectando ao banco de dados")
