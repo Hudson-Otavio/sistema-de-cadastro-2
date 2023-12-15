f33c6055cfe3db86043d8d48bdf5d83fd46c6a79
Hudson <hudsonpereira1@gmail.com>
2023-12-15 09:50:27 -0300

    corrigindo o app js

--- app.js
+++ app.js
@@ -2,5 +2,4 @@ console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("aplicalçao está se conectando ao banco de dados")
 //mais um console de teste:
 console.log("websis fez um commit sem você ver")
-console.log("Enviando para o GitHub")
-console.log("Exemplo de mais um código")
+
